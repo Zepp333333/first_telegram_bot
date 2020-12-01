f765a0a2aecc2009c5eb7d4158dc1d77b4d54c96
Sergei Sazonov <sergesazonov@gmail.com>
2020-12-01 17:00:39 +0300

    first conversation v0.02

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ import error_handler
 import keyboards
 
 
-
-
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
@@ -25,31 +23,39 @@ logger = logging.getLogger(__name__)
 CHOOSE_ACTION = chr(1)
 GATHER_SELF_INFO, GATHER_TARGET_ATHL_INFO = map(chr, range(2, 4))
 IN_SEARCH_FOR_TEAM, IN_SEARCH_FOR_ATHLETE = map(chr, range(4, 6))
-SWIMMER, BIKER, RUNNER = map(chr, range(15, 18))
+SWIMMER, BIKER, RUNNER = ('swimmer', 'biker', 'runner')
 GOT_SELF_ROLE = map(chr, range(20, 21))
 EVENT_SELECTION = map(chr, range(30, 31))
+VIEW_OPTIONS = map(chr, range(40, 41))
+
 
 # State definitions nested conversations
 # todo
 
 # Meta States
-STOPPING = map(chr, range(99, 100))
+BACK, STOPPING = map(chr, range(98, 100))
 
 # Other Constants
 START_OVER = map(chr, range(100, 101))
-
 
 # Shortcut for ConversationHandler.END
 END = ConversationHandler.END
 
 
+translation_dict = {
+    'swimmer': 'пловец 🏊',
+    'biker': 'велосипедист 🚴',
+    'runner': 'бегун 🏃',
+}
+
+
 def start(update: Update, context: CallbackContext):
     """Select an action: Search for Team or an Athlete"""
-    logger.info("User %s got into /start with command %s",
-                update.message.from_user.first_name, update.message.text)
+
     text = (
-        'Привет! Я помогаю найти недостающего атлета '
-        'или целую команду для участия в триатлонной эстафете. '
+        'Привет! Я бот - помогаю найти недостающего атлета '
+        'или целую команду для участия в триатлонной эстафете. \n'
+        'Чтобы прекратить это безобразие в любой момент нажми /stop \n '
         'Что будем делать?'
     )
     buttons = [
@@ -59,19 +65,21 @@ def start(update: Update, context: CallbackContext):
         ]
     ]
     keyboard = InlineKeyboardMarkup(buttons)
-    update.message.reply_text(text=text, reply_markup=keyboard)
 
-    # # If we're starting over we don't need do send a new message
-    # if context.user_data.get(START_OVER):
-    #     update.callback_query.answer()
-    #     update.callback_query.edit_message_text(text=text, reply_markup=keyboard)
-    # else:
-    #     update.message.reply_text(
-    #         'Hi, I\'m FamiliyBot and here to help you gather information' 'about your family.'
-    #     )
-    #     update.message.reply_text(text=text, reply_markup=keyboard)
+    # If we're starting over we don't need do send a new message
+    if context.user_data.get(START_OVER):
+        user = update.callback_query.from_user
+        update.callback_query.answer()
+        update.callback_query.edit_message_text(text=text, reply_markup=keyboard)
+    else:
+        user = update.message.from_user
+        update.message.reply_text(text=text, reply_markup=keyboard)
 
     context.user_data[START_OVER] = False
+
+    logger.info('User %s got into /start with command', user)
+    logger.info('userdata %s', context.user_data)
+    print('state=CHOOSE_ACTION')
     return CHOOSE_ACTION
 
 
@@ -79,9 +87,11 @@ def self_role(update: Update, context: CallbackContext):
 
     logger.info("User %s is searching for a team in %s",
                 update.callback_query.from_user.first_name, "self_role")
+    logger.info('userdata %s', context.user_data)
+
     context.user_data['goal'] = IN_SEARCH_FOR_TEAM
     text = (
-        'Ок, ищем команду.\n '
+        'Ок, ищем команду.\n'
         'Расскажи о себе. '
     )
     buttons = [
@@ -89,23 +99,29 @@ def self_role(update: Update, context: CallbackContext):
             InlineKeyboardButton(text='Я пловец', callback_data=str(SWIMMER)),
             InlineKeyboardButton(text='Я велосипедист', callback_data=str(BIKER)),
             InlineKeyboardButton(text='Я бегун', callback_data=str(RUNNER)),
+        ],
+        [
+            InlineKeyboardButton(text='Назад', callback_data=str(END)),
         ]
     ]
     keyboard = InlineKeyboardMarkup(buttons)
     update.callback_query.edit_message_text(text=text, reply_markup=keyboard)
 
+    context.user_data['level'] = GATHER_SELF_INFO
+    print('state=GOT_SELF_ROLE')
     return GOT_SELF_ROLE
-
 
 
 def select_event(update: Update, context: CallbackContext):
     logger.info('User %s got to %s',
                 update.callback_query.from_user.first_name, 'select_event')
+    logger.info('userdata %s', context.user_data)
+
     context.user_data['role'] = update.callback_query.data
 
     text = (
-        'Ок, ищем команду.\n '
-        'Ты ' + str(context.user_data['role']) + '\n'  #Todo - make sure we print user role
+        'Ок, ищем команду.\n'
+        'Ты ' + translation_dict[(context.user_data['role'])] + '\n' 
         'В какой гонке ты хочешь участовоать?'
     )
     buttons = [
@@ -113,10 +129,16 @@ def select_event(update: Update, context: CallbackContext):
             InlineKeyboardButton(text='Гонка 1', callback_data=str(1)),
             InlineKeyboardButton(text='Гонка 2', callback_data=str(2)),
             InlineKeyboardButton(text='Гонка 3', callback_data=str(3)),
+        ],
+        [
+            InlineKeyboardButton(text='Назад', callback_data=str(END)),
         ]
     ]
     keyboard = InlineKeyboardMarkup(buttons)
     update.callback_query.edit_message_text(text=text, reply_markup=keyboard)
+    context.user_data['level'] = EVENT_SELECTION
+    print('state = VIEW_OPTIONS ')
+    return VIEW_OPTIONS
 
 def in_search_for_athlete(update: Update, context: CallbackContext):
     # TODO: STUB
@@ -124,23 +146,44 @@ def in_search_for_athlete(update: Update, context: CallbackContext):
     return END
 
 
+def go_back(update: Update, context: CallbackContext):
+    logger.info('User %s got to %s',
+                update.callback_query.from_user.first_name, 'go_back')
+    logger.info('userdata %s', context.user_data)
+
+    level = context.user_data['level']
+    if level == GATHER_SELF_INFO:
+        context.user_data[START_OVER] = True
+        start(update, context)
+        print('state=END')
+        return END
+    elif level == EVENT_SELECTION:
+        self_role(update, context)
+        print('state=IN_SEARCH_FOR_TEAM')
+        return GOT_SELF_ROLE
+
 
 
 def stop(update: Update, context: CallbackContext):
     """End Conversation by command."""
     update.message.reply_text('До встречи!')
+    print('state = END')
     return END
+
 
 def stop_nested(update: Update, context: CallbackContext) -> None:
     """Completely end conversation from within nested conversation."""
     update.message.reply_text('Okay, bye.')
+    print('state = STOPPING')
     return STOPPING
+
 
 def end(update: Update, context: CallbackContext) -> None:
     """End conversation from InlineKeyboardButton."""
     update.callback_query.answer()
     text = 'До встречи!'
     update.callback_query.edit_message_text(text=text)
+    print('state = END')
     return END
 
 
@@ -154,13 +197,21 @@ def main():
     find_team_conv = ConversationHandler(
         entry_points=[CallbackQueryHandler(self_role, pattern='^' + str(IN_SEARCH_FOR_TEAM) + '$')],
         states={
+            VIEW_OPTIONS: [CallbackQueryHandler(select_event,
+                                                 pattern=
+                                                 '^' + str(SWIMMER) + '$|^'
+                                                 + str(BIKER) + '$|^'
+                                                 + str(RUNNER) + '$')],
             GOT_SELF_ROLE: [CallbackQueryHandler(select_event,
                                                  pattern=
                                                  '^' + str(SWIMMER) + '$|^'
                                                  + str(BIKER) + '$|^'
                                                  + str(RUNNER) + '$')],
         },
-        fallbacks=[CommandHandler('stop', stop_nested)],
+        fallbacks=[
+            CallbackQueryHandler(go_back, pattern='^' + str(END) + '$'),
+            CommandHandler('stop', stop_nested)
+        ],
         map_to_parent={
             END: CHOOSE_ACTION,
             STOPPING: END
@@ -181,11 +232,15 @@ def main():
     main_conv_handler = ConversationHandler(
         entry_points=[CommandHandler('Start', start)],
         states={
+            GOT_SELF_ROLE: [CallbackQueryHandler(start, pattern='^' + str(END) + '$')],
             CHOOSE_ACTION: [find_team_conv, find_athlete_conv],
             # todo show info
-            STOPPING: [CommandHandler('start', start)]
+            STOPPING: [CommandHandler('start', start)],
         },
-        fallbacks=[CommandHandler('stop', stop)],
+        fallbacks=[
+            CommandHandler('stop', stop),
+            # CommandHandler('start', start),
+        ]
     )
 
 
