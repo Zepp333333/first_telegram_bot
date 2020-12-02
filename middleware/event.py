6fb79aa750f2cff871fced7321e20656a544bd88
Sergei Sazonov <sergesazonov@gmail.com>
2020-12-02 20:55:39 +0300

    IronStar Scraper v0.2

--- middleware/event.py
+++ middleware/event.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import re
 import uuid
 
+
 class TriathlonEvent(object):
     """
     Class representing triathlon event including their Title,
@@ -32,6 +33,9 @@ class TriathlonEvent(object):
         if image_url:
             self.image_url = image_url
 
+    def __str__(self):
+        return
+
 
 class IronStartEvent(TriathlonEvent):
     ironstar_base_url = 'https://iron-star.com'
@@ -42,24 +46,19 @@ class IronStartEvent(TriathlonEvent):
 
     def pupulate_event_data(self, event):
         self.add_data(
-            url=ironstar_base_url + event.attrs.get('href'),
+            url=IronStartEvent.ironstar_base_url + event.attrs.get('href'),
             title=event.find('div', {'class': 'title'}).get_text().strip(),
             date=event.find('div', {'class': 'date'}).get_text().strip(),
             location=event.find('div', {'class': 'place'}).get_text().strip(),
-            # image_url=ironstar_base_url + re.search(
-            #               r'(\/.*)\);',
-            #               event.find('div', {'class': 'image'})['style']
-            #           ).group(1)
         )
 
         # making-up image url
         # todo extract method/function
         div_url = event.find('div', {'class': 'image'})['style']
-        image_url = ironstar_base_url + re.search(r'(\/.*)\);', div_url).group(1)
+        image_url = IronStartEvent.ironstar_base_url + re.search(r'(\/.*)\);', div_url).group(1)
         self.add_data(image_url=image_url)
 
-
-        # making - up distances
+        # making-up distances
         # todo extract method/function
         event_race_distance = {}
         distances = event.find_all('div', {'class': 'triathlon-item'})
@@ -73,9 +72,21 @@ class IronStartEvent(TriathlonEvent):
             event_race_distance[tag] = text
         self.add_data(race_distance=event_race_distance)
 
+    def __str__(self):
+        if self.race_distance:
+            distance = []
+            for k, v in self.race_distance.items():
+                distance.append(k)
+                distance.append(v)
+                distance.append('|')
+            distance = ' '.join(distance)
 
 
-ironstar_base_url = 'https://iron-star.com'
+        return str(f'{self.location} {self.title} {self.date}\n'
+                   f'{distance}\n'
+                   f'{self.url}\n')
+
+
 ironstar_event_list_url = 'https://iron-star.com/event/'
 response = requests.get(ironstar_event_list_url)
 html = response.content
@@ -87,6 +98,11 @@ for event in soup.find_all('a', {'class':'event-item'}):
     event_list.append(event)
 
 # example event
-event = event_list[-1]
+# event = event_list[-1]
+all_events = []
+for event in event_list:
+    i = IronStartEvent()
+    i.pupulate_event_data(event)
+    all_events.append(i)
 
 
